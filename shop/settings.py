5f564c5ee7bbc0eb85735339dddfce1796678e15
21111990 <87606783+21111990@users.noreply.github.com>
2021-07-23 20:09:17 +0530

    missiontwok

--- shop/settings.py
+++ shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$rja*6%um23(&8jzu)(l7fx58%db94uje_4f@k&wahz&b+qsqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','missionmtkcheck.herokuapp.com']
 
